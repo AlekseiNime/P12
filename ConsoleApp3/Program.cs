9ecfbaafe797f2bc65562d789f3e1bfe2cfa856d
Сардак Алексей <141231008+AlekseiNime@users.noreply.github.com>
2024-12-10 09:19:51 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Vvod("Month= ");
+            string x = GetName(n);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+        public static int Vvod(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static string GetName(int month)
+        {
+            switch(month)
+            {
+                case 1: return "Январь";
+                case 2: return "февраль";
+                case 3: return "март";
+                case 4: return "апрель";
+                case 5: return "май";
+                case 6: return "июнь";
+                case 7: return "июль";
+                case 8: return "август";
+                case 9: return "сентябрь";
+                case 10: return "октябрь";
+                case 11: return "ноябрь";
+                case 12: return "декабрь";
+
+                default:return "Ошибка";
+
+
+            }
+        }
+    }
+}
