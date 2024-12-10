a60fe23dbbf9f857897cfc80425d78a2ead97538
Сардак Алексей <141231008+AlekseiNime@users.noreply.github.com>
2024-12-10 09:37:51 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Vvod("Т/С= ");
+            string x = GetName(n);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+        public static string Vvod(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        static string GetName(string letter)
+        {
+            switch (letter.ToLower())
+            {
+                case "а": return "автомобиль - 180 км.ч";
+                case "в": return "велосепед - 20 км.ч";
+                case "м": return "мотоцикл - 200 км.ч";
+                case "с": return "самолёт - 420 км.ч";
+                case "п": return "поезд - 100 км.ч";
+                default: return "Не найдено";
+            }
+        }
+    }
+}
